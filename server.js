93f9e4526fae4ab282c931b0f55cc8558fe4cbbd
emventure <admin.emventures@gmail.com>
2026-07-07 20:10:48 +0530

    Fix SMTP port assignment in server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const SMTP_PASS = process.env.SMTP_PASS;
 
 const transporter = nodemailer.createTransport({
   host:process.env.SMTP_HOST,
-  port:process.env.SMTP_PORT)
+  port:Number(process.env.SMTP_PORT)
   secure:false,
   auth: { user:process.env.SMTP_USER, pass:process.env.SMTP_PASS }
 });
